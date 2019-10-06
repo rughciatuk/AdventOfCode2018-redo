ab453a8504856cfde86cb372d22c01b8d33a4854
unknown <Roi Ben - Shaul>
2019-10-06 11:59:40 +0300

    Add day 1 part 2

--- day1/day1.py
+++ day1/day1.py
@@ -1,7 +1,29 @@
-with open("input.txt", "r") as input_file:
-    data = input_file.readlines()
+INPUT = "input.txt"
+
+def part_1(input_name):
+    with open(input_name, "r") as input_file:
+        data = input_file.readlines()
+        curr_freq = 0
+        for freq in map(lambda x: int(x.replace("\n", "")), data):
+            curr_freq += freq
+        return curr_freq
+
+def part_2(input_name):
+    with open(input_name, "r") as input_file:
+        data = list(map(lambda x: int(x.replace("\n", "")), input_file.readlines()))
+
+    old_freqs = set()
     curr_freq = 0
-    for freq in map(lambda x: int(x.replace("\n", "")), data):
-        curr_freq += freq
-    print("Curr freq:", curr_freq)
-    
+    freq_index = 0
+    while curr_freq not in old_freqs:
+        old_freqs.add(curr_freq)
+        curr_freq += data[freq_index % len(data)]
+        freq_index += 1
+
+    return curr_freq
+
+
+if __name__ == "__main__":
+    print("part 1:", part_1(INPUT))
+    print("part 2:", part_2(INPUT))
+
